c349d84f163eb3bdfc3571f02bd35ba2962ab1e2
Abhinav_Jha <abhi.mount.26@gmail.com>
2022-05-19 23:27:45 +0530

    parcel updated to v2

--- src/Container/Tours/Tours.js
+++ src/Container/Tours/Tours.js
@@ -1,88 +1,75 @@
-import React, { useEffect, useState } from "react";
-import "./Tours.css"
-import Loading from "./../../Components/Loading/Loading";
-import Tour from "../../Components/Tour/Tour";
-import Axios from "axios";
-const url = "https://course-api.com/react-tours-project";
+import React, { useEffect, useState } from 'react'
+import './Tours.css'
+import Loading from './../../Components/Loading/Loading'
+import Tour from '../../Components/Tour/Tour'
+import Axios from 'axios'
+const url = 'https://course-api.com/react-tours-project'
 
 function tours() {
-    const [loading, setLoading] = useState(true);
-    const [tours, setTours] = useState([]);
-    const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+  const [error, setError] = useState(false)
 
-    const fetchTours = () => {
-        setLoading(true)
-        Axios.get(url)
-            .then((res) => {
-                setLoading(false);
-                setTours(res.data);
+  const fetchTours = () => {
+    setLoading(true)
+    Axios.get(url)
+      .then((res) => {
+        setLoading(false)
+        setTours(res.data)
+      })
+      .catch((error) => {
+        console.log(error)
+        setLoading(false)
+        setError(true)
+      })
+  }
 
-            })
-            .catch((error) => {
-                console.log(error);
-                setLoading(false);
-                setError(true)
-            });
-    }
+  useEffect(() => void fetchTours(), [])
 
-    useEffect(() => {
-        fetchTours()
-        return () => console.log(tours)
-    }, []);
+  const removeTourHandler = (id, index) => {
+    const updatedTours = tours.filter((tour) => tour.id !== id)
+    setTours(updatedTours)
+  }
 
-    const removeTourHandler = (id, index) => {
-        const updatedTours = tours.filter(tour => tour.id !== id)
-        // const updatedTours = tours
-        // updatedTours.splice(index, 1)   //-->This is not Working and don't know why??
-        console.log(updatedTours)
-        setTours(updatedTours)
-        console.log("clicked!!", index)
-    }
-
-    if (loading) {
-        return (
-            <div>
-                <Loading />
-            </div>
-        );
-    }
-    if (error) {
-        return (
-            <header className="title">
-                <h1>Sorry ! Network Problem</h1>
-            </header>
-        )
-    }
-    if (tours.length === 0) {
-        return (
-            <header className="title">
-                <h1>No Tours Available!</h1>
-                <button className="refresh" onClick={() => fetchTours()}>Refresh!</button>
-            </header>
-        )
-    }
-    else {
-
-        const updatedTours = tours
-        const Tours = updatedTours.map((tour, tourIndex) => {
-            return (
-                <Tour key={tour.id}
-                    tour={tour}
-                    removeTour={() => removeTourHandler(tour.id, tourIndex)}
-                />
-            )
-        })
-        return (
-            <main className="tours">
-                <header className="title">
-                    <h1>Our Tours</h1>
-                    <div className="underline"></div>
-                </header>
-                <section>
-                    {Tours}
-                </section>
-            </main>
-        );
-    }
-};
-export default tours;
+  if (loading) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <header className='title'>
+        <h1>Sorry ! Network Problem</h1>
+      </header>
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <header className='title'>
+        <h1>No Tours Available!</h1>
+        <button className='refresh' onClick={() => fetchTours()}>
+          Refresh!
+        </button>
+      </header>
+    )
+  } else {
+    const updatedTours = tours
+    const Tours = updatedTours.map((tour, tourIndex) => {
+      return (
+        <Tour key={tour.id} tour={tour} removeTour={() => removeTourHandler(tour.id, tourIndex)} />
+      )
+    })
+    return (
+      <main className='tours'>
+        <header className='title'>
+          <h1>Our Tours</h1>
+          <div className='underline'></div>
+        </header>
+        <section>{Tours}</section>
+      </main>
+    )
+  }
+}
+export default tours
--- src/Container/Tours/Tours1.js
+++ src/Container/Tours/Tours1.js
@@ -1,92 +1,93 @@
 import React, { Component } from 'react'
-import "./Tours.css"
-import Loading from "../../Components/Loading/Loading";
-import Tour from "../../Components/Tour/Tour";
-import Axios from "axios";
-const url = "https://course-api.com/react-tours-project";
+import './Tours.css'
+import Loading from '../../Components/Loading/Loading'
+import Tour from '../../Components/Tour/Tour'
+import axios from 'axios'
+const url = 'https://course-api.com/react-tours-project'
 
 class Tours extends Component {
-    state = {
-        tours: [],
-        loading: true,
-        error: false
-    }
-    fetchTours = () => {
+  state = {
+    tours: [],
+    loading: true,
+    error: false,
+  }
+  fetchTours = () => {
+    this.setState({
+      loading: true,
+    })
+    axios
+      .get(url)
+      .then((res) => {
         this.setState({
-            loading: true
+          loading: false,
+          tours: res.data,
         })
-        Axios.get(url)
-            .then((res) => {
-                this.setState({
-                    loading: false,
-                    tours: res.data
-                })
-            })
-            .catch((error) => {
-                console.log(error.message);
-                this.setState({
-                    loading: false,
-                    error: true
-                })
-            });
-    }
-    componentDidMount() {
-        this.fetchTours()
-    }
-    removeTourHandler = (id, index) => {
-        // const updatedTours = tours.filter(tour => tour.id !== id)
-        const updatedTours = this.state.tours
-        updatedTours.splice(index, 1)    //-->But,works properly here
+      })
+      .catch((error) => {
+        console.log(error.message)
         this.setState({
-            tours: updatedTours
+          loading: false,
+          error: true,
         })
-        console.log("clicked!!", index)
+      })
+  }
+  componentDidMount() {
+    this.fetchTours()
+  }
+  removeTourHandler = (id, index) => {
+    // const updatedTours = tours.filter(tour => tour.id !== id)
+    const updatedTours = this.state.tours
+    updatedTours.splice(index, 1) //-->But,works properly here
+    this.setState({
+      tours: updatedTours,
+    })
+    console.log('clicked!!', index)
+  }
+  render() {
+    if (this.state.loading) {
+      return (
+        <div>
+          <Loading />
+        </div>
+      )
     }
-    render() {
-        if (this.state.loading) {
-            return (
-                <div>
-                    <Loading />
-                </div>
-            );
-        }
-        if (this.state.error) {
-            return (
-                <header className="title">
-                    <h1>Sorry ! Network Problem</h1>
-                </header>
-            )
-        }
-        if (this.state.tours.length === 0) {
-            return (
-                <header className="title">
-                    <h1>No Tours Available!</h1>
-                    <button className="refresh" onClick={() => this.fetchTours()}>Refresh!</button>
-                </header>
-            )
-        }
-        else {
-            const updatedTours = this.state.tours
-            const Tours = updatedTours.map((tour, tourIndex) => {
-                return (
-                    <Tour key={tour.id}
-                        tour={tour}
-                        removeTour={() => this.removeTourHandler(tour.id, tourIndex)}
-                    />
-                )
-            })
-            return (
-                <main className="tours">
-                    <header className="title">
-                        <h1>Our Tours</h1>
-                        <div className="underline"></div>
-                    </header>
-                    <section>
-                        {Tours}
-                    </section>
-                </main>
-            );
-        }
+    if (this.state.error) {
+      return (
+        <header className='title'>
+          <h1>Sorry ! Network Problem</h1>
+        </header>
+      )
     }
+    if (this.state.tours.length === 0) {
+      return (
+        <header className='title'>
+          <h1>No Tours Available!</h1>
+          <button className='refresh' onClick={() => this.fetchTours()}>
+            Refresh!
+          </button>
+        </header>
+      )
+    } else {
+      const updatedTours = this.state.tours
+      const Tours = updatedTours.map((tour, tourIndex) => {
+        return (
+          <Tour
+            key={tour.id}
+            tour={tour}
+            removeTour={() => this.removeTourHandler(tour.id, tourIndex)}
+          />
+        )
+      })
+      return (
+        <main className='tours'>
+          <header className='title'>
+            <h1>Our Tours</h1>
+            <div className='underline'></div>
+          </header>
+          <section>{Tours}</section>
+        </main>
+      )
+    }
+  }
 }
 export default Tours
